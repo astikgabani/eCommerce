0d8f768cddb322efaf454183678d675a314b93ae
Astik Gabani <gabaniastik@gmail.com>
2020-06-23 16:30:38 +0530

    Changing some deployment configs to solve the issue

--- app.py
+++ app.py
@@ -6,15 +6,12 @@ from flask_uploads import configure_uploads, patch_request_class
 from flask_jwt_extended import JWTManager
 from flask_login import login_user, logout_user, LoginManager, current_user
 
-from models.users import UserModel, UserRoleModel
+from models.users import UserModel
 
 from dotenv import load_dotenv
 from marshmallow import ValidationError
 
 from plugins.admin import admin
-from plugins.db import db
-from plugins.ma import ma
-from plugins.mail import mail
 
 from add_resources import add_resources
 from add_admin_views import add_admin_views
@@ -45,25 +42,6 @@ def validation_error(error):
 @app.errorhandler(AssertionError)
 def validation_error(error):
     return {"Error": str(error)}, 400
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-    if UserRoleModel.get_item(role="admin"):
-        return
-    user_role = UserRoleModel()
-    user_role.role = "admin"
-    user_role.save_to_db()
-    user = UserModel()
-    user.email = "admin_test@yopmail.com"
-    user.password = "admin123"
-    user.first_name = "admin"
-    user.last_name = "admin"
-    user.phone_no = 9999999999
-    user.save_to_db()
-    user.roles.append(user_role)
-    user.save_to_db()
 
 
 @login_manager.user_loader
@@ -110,8 +88,4 @@ add_admin_views(admin)
 
 
 if __name__ == "__main__":
-    db.init_app(app)
-    ma.init_app(app)
-    mail.init_app(app)
-    admin.init_app(app)
     app.run(port=7000)
---
+++ wsgi.py
@@ -0,0 +1,33 @@
+from app import app as application
+
+from models.users import UserModel, UserRoleModel
+
+from plugins.db import db
+from plugins.ma import ma
+from plugins.mail import mail
+from plugins.admin import admin
+
+
+db.init_app(application)
+ma.init_app(application)
+mail.init_app(application)
+admin.init_app(application)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    if UserRoleModel.get_item(role="admin"):
+        return
+    user_role = UserRoleModel()
+    user_role.role = "admin"
+    user_role.save_to_db()
+    user = UserModel()
+    user.email = "admin_test@yopmail.com"
+    user.password = "admin123"
+    user.first_name = "admin"
+    user.last_name = "admin"
+    user.phone_no = 9999999999
+    user.save_to_db()
+    user.roles.append(user_role)
+    user.save_to_db()
