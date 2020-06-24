a0bd2cc19d3692ceabd6a538718f2e6498ea22a1
Astik Gabani <gabaniastik@gmail.com>
2020-06-24 12:06:40 +0530

    Changed deployment settings

--- config/default_config.py
+++ config/default_config.py
@@ -3,7 +3,7 @@ import os
 DEBUG = True
 ENV = "development"
 TESTING = True
-SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///data.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PROPAGATE_EXCEPTIONS = True
 JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
