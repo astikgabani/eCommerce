97e52e4272563127fdcb7cf29f35d20423077a34
Astik Gabani <gabaniastik@gmail.com>
2020-07-18 09:28:48 +0530

    Added method string for users resource

--- package/resources/users.py
+++ package/resources/users.py
@@ -43,81 +43,111 @@ confirmation_schema = UserConfirmationSchema()
 class UserLogin(Resource):
     @classmethod
     def post(cls):
+        """
+        Verify the credentials and return tokens
+        1. load the request data
+        2. if user not found, return user not registered 401
+        3. verify the credentials, if not authorized, return invalid creds 401
+        4. check for email confirmation, if not confirmed, return email not confirm 400
+        5. generate the access-token and refresh-token
+        6. load the session info
+        7. if new IP is found for user for specific type then automatically logout from previous sessions
+        8. if more than 5 session has generated, automatically logout from previous sessions
+        9. Register new token into db
+        10. return newly created access-token and refresh-token.
+
+        @return: return newly created access-token and refresh-token.
+        @rtype: dict of tokens
+        """
         req_data = request.get_json()
         user = user_login_schema.load(
             req_data,
             instance=UserModel.get_item(email=req_data.get("email")),
             partial=True,
         )
-        if user.id:
-            input_password = req_data.get("clear_input_password")
-            if user.verify_password(input_password):
-                confirmation = user.most_recent_confirmation
-                if not (confirmation and confirmation.confirmed):
-                    return {"message": gettext("user_email_not_confirmed")}, 400
+        if not user.id:
+            return {"message": gettext("user_not_found")}, 401
 
-                # Generate tokens
-                access_token = create_access_token(identity=user.id, fresh=True)
-                refresh_token = create_refresh_token(user.id)
-                note = None
-
-                # Loading session info
-                session_instance = UserSessionModel.get_item(
-                    user=user, type=req_data.get("type") or "web"
-                )
-                current_session = user_session_schema.load(
-                    req_data, instance=session_instance, partial=True
-                )
-
-                # Manage sessions
-                # if new IP is found for user for specific type then automatically logout from previous sessions
-                # if more than 5 session has generated, automatically logout from previous sessions
-                if current_session.id and (
-                    current_session.ip != request.remote_addr
-                    or len(current_session.tokens) >= 5
-                ):
-                    for session_token in current_session.tokens:
-                        session_token.deactivate()
-                    current_session.deactivate()
-                    current_session.save_to_db()
-                    current_session = user_session_schema.load(req_data)
-                note = gettext("user_login_max_count_reached")
-
-                current_session.user = user
-                current_session.ip = request.remote_addr
-                current_session.save_to_db()
-
-                # Generate new session
-                if refresh_token not in (
-                    token.refresh_token for token in current_session.tokens
-                ):
-                    session_token_dict = {
-                        "refresh_token": refresh_token,
-                        "session_id": current_session.id,
-                    }
-                    token = user_session_token_schema.load(session_token_dict)
-                    token.save_to_db()
-
-                total_session_note = gettext("user_active_sessions").format(
-                    len(current_session.tokens)
-                )
-                note = note + total_session_note if note else total_session_note
-
-                return (
-                    {
-                        "access_token": access_token,
-                        "refresh_token": refresh_token,
-                        "note": note,
-                    },
-                    200,
-                )
+        input_password = req_data.get("clear_input_password")
+        if not user.verify_password(input_password):
             return {"message": gettext("user_invalid_credentials")}, 401
-        return {"message": gettext("user_not_found")}, 401
+
+        confirmation = user.most_recent_confirmation
+        if not (confirmation and confirmation.confirmed):
+            return {"message": gettext("user_email_not_confirmed")}, 400
+
+        # Generate tokens
+        access_token = create_access_token(identity=user.id, fresh=True)
+        refresh_token = create_refresh_token(user.id)
+        note = None
+
+        # Loading session info
+        session_instance = UserSessionModel.get_item(
+            user_id=user, type=req_data.get("type") or "web"
+        )
+        current_session = user_session_schema.load(
+            req_data, instance=session_instance, partial=True
+        )
+
+        # Manage sessions
+        # if new IP is found for user for specific type then automatically logout from previous sessions
+        # if more than 5 session has generated, automatically logout from previous sessions
+        if current_session.id and (
+            current_session.ip != request.remote_addr
+            or len(current_session.tokens) >= 5
+        ):
+            for session_token in current_session.tokens:
+                session_token.deactivate()
+            current_session.deactivate()
+            current_session.save_to_db()
+            current_session = user_session_schema.load(req_data)
+        note = gettext("user_login_max_count_reached")
+
+        current_session.user = user
+        current_session.ip = request.remote_addr
+        current_session.save_to_db()
+
+        # register new token into DB
+        if refresh_token not in (
+            token.refresh_token for token in current_session.tokens
+        ):
+            session_token_dict = {
+                "refresh_token": refresh_token,
+                "session_id": current_session.id,
+            }
+            token = user_session_token_schema.load(session_token_dict)
+            token.save_to_db()
+
+        total_session_note = gettext("user_active_sessions").format(
+            len(current_session.tokens)
+        )
+        note = note + total_session_note if note else total_session_note
+
+        return (
+            {
+                "access_token": access_token,
+                "refresh_token": refresh_token,
+                "note": note,
+            },
+            200,
+        )
 
 
 class UserRegister(Resource):
     @classmethod
     def post(cls):
+        """
+        Register the user
+        1. load the user with request data
+        2. if user already registered, return 409 conflict
+        3. save user to db, load the confirmation with user_id and save confirmation to db
+        4. send the confirmation mail
+        5. return the newly created user details
+        6. if any exception occurred, delete user from db
+
+        @return: the newly created user details
+        @rtype: dict of newly created user details
+        """
         req_data = request.get_json()
         user = user_register_schema.load(
             req_data,
@@ -157,6 +187,17 @@ class UserTokenRefresh(Resource):
     @classmethod
     @jwt_refresh_token_required
     def post(cls):
+        """
+        Create a new access-token from refresh-token
+        1. extract refresh-token from header
+        2. load the session from db
+        3. load the token from db
+        4. if token found, create access-token and return it.
+        5. else return not found 404
+
+        @return: newly created access-token
+        @rtype: 200
+        """
         current_user = get_jwt_identity()
         auth_header = request.headers.get("Authorization")
         user_token = auth_header.split()[1]
@@ -189,6 +230,16 @@ class UserFreshLogin(Resource):
 
     @classmethod
     def post(cls):
+        """
+        Generate new fresh access-token from creds
+        1. load the user details with request data
+        2. if user not found, return 404 not found
+        3. verify the creds, if creds not verified, return invalid credentials, 401
+        4. generate and return new fresh access-token
+
+        @return: new fresh access-token
+        @rtype: dict of access-token
+        """
         req_data = request.get_json()
         user = user_login_schema.load(
             req_data,
@@ -199,24 +250,44 @@ class UserFreshLogin(Resource):
             return {"message": gettext("user_not_found")}, 401
 
         input_password = req_data.get("clear_input_password")
-        if user.verify_password(input_password):
-            return (
+        if not user.verify_password(input_password):
+            return {"message": gettext("user_invalid_credentials")}, 401
+        return (
                 {"access_token": create_access_token(identity=user.id, fresh=True)},
                 200,
             )
-        return {"message": gettext("user_invalid_credentials")}, 401
 
 
 class UserRoles(Resource):
     @jwt_required
     @required_role(["admin"])
     def get(self):
+        """
+        Get all the roles
+        1. fetch all the roles from the db
+        2. if roles not found, return 404 not found
+        3. return list of all roles
+
+        @return: all the roles
+        @rtype: dict of roles list
+        """
         roles = UserRoleModel.get_items()
+        if not roles:
+            return {"message": gettext("user_role_not_found")}, 404
         return {"roles": [role.role for role in roles]}, 200
 
     @jwt_required
     # @required_role(["admin"])
     def post(self):
+        """
+        Create the role
+        1. load the role with request data from the db
+        2. if role found, return 409 conflict
+        3. save to db and return newly created role details
+
+        @return: newly created role details
+        @rtype: dict of role details
+        """
         req_data = request.get_json()
         user_role = user_role_schema.load(
             req_data, instance=UserRoleModel.get_item(role=req_data.get("role"))
@@ -237,6 +308,16 @@ class UserRoleAssign(Resource):
     @jwt_required
     # @required_role(["admin"])
     def post(self):
+        """
+        Assign the specific roles to user
+        1. load the user role with request data from the db
+        2. if role found, return 409 conflict
+        3. loop through all the user emails. load the user from db and add user into the user_role's object.
+        4. save the user roles and return success, already assigned and user not found list
+
+        @return: success, already assigned and user not found list
+        @rtype: dict of success, already assigned and user not found list
+        """
         req_data = request.get_json()
         user_role = user_role_schema.load(
             req_data, instance=UserRoleModel.get_item(role=req_data.get("role"))
@@ -273,6 +354,19 @@ class UserRoleAssign(Resource):
 class UserEmailConfirm(Resource):
     @classmethod
     def get(cls, confirmation_id: str):
+        """
+        @param confirmation_id: confirmation id
+        @type confirmation_id: hex string
+
+        Confirm the user.
+        1. fetch the confirmation details from the db
+        2. if confirmation expired, return 400 bad request
+        3. if confirmation already confirmed, return 409 conflict
+        4. mark the confirm flag of user, save to db and return proper msg
+
+        @return: return user confirmation msg
+        @rtype: 200
+        """
         confirmation = UserConfirmationModel.get_item(id=confirmation_id)
         if not confirmation:
             return {"message": gettext("user_not_found")}, 404
@@ -313,7 +407,14 @@ class UserEmailConfirmByUser(Resource):
 
     def post(self, user_id: int):
         """
-        resend confirmation email
+        resend confirmation email.
+        1. fetch the user details from db
+        2. if user not found, return 404 not found
+        3. get the recent confirmation, check the confirm flag, if it's true, return 409 conflict
+        4. load the confirmation chema, save to db and send confirmation mail
+        5. return success msg
+        6. If any exceptions occurred, return error msg
+
         @param user_id:
         @return:
         """
@@ -324,11 +425,11 @@ class UserEmailConfirmByUser(Resource):
             confirmation = user.most_recent_confirmation
             if confirmation:
                 if confirmation.confirmed:
-                    return {"message": gettext("user_already_confirmed")}, 400
+                    return {"message": gettext("user_already_confirmed")}, 409
 
             new_confirmation = confirmation_schema.load({"user_id": user_id})
             new_confirmation.save_to_db()
             user.send_confirmation_email()
-            return {"message": gettext("user_email_resend_successfully")}
+            return {"message": gettext("user_email_resend_successfully")}, 200
         except Exception as e:
             return {"message": str(e)}, 500
